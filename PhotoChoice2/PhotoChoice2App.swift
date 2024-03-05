c6613023d62911a249e28dacf886371c49265938
Garrett Post <Gep100iosgames@gmail.com>
2024-03-04 19:35:30 -0500

    Initial Commit

---
+++ PhotoChoice2/PhotoChoice2App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoChoice2App.swift
+//  PhotoChoice2
+//
+//  Created by Garrett Post on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoChoice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
